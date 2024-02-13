85b08919acd09e7432dafa3d3f10533bcd134555
Abed Rass <abd.assoum@outlook.com>
2024-02-13 20:10:03 +0100

    P

--- src/main/java/org/example/PasswordGenerator.java
+++ src/main/java/org/example/PasswordGenerator.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class PasswordGenerator {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
